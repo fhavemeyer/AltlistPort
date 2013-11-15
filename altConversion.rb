0e879861741dc78caa7ad884f44fa14111173d0c
Frederick Havemeyer <frederick.havemeyer@gmail.com>
2013-11-15 00:06:42 -0500

    Now querying each set of associated alts at once as I should have done before

--- altConversion.rb
+++ altConversion.rb
@@ -11,6 +11,8 @@ uri = URI.parse("https://api.mojang.com/profiles/page/1")
 
 output = File.new("uuid_list.txt", "w")
 
+associations = 1
+
 if (alt_list != nil)
 	if (File.exist?(alt_list))
 		puts "Parsing #{alt_list}"
@@ -18,31 +20,29 @@ if (alt_list != nil)
 		File.open(alt_list).each do |line|
 			linked_uuids = Array.new
 
-			line.split.each do |account|
-				json_body = JSON.generate({'agent' => 'minecraft', 'name' => account})
+			query_list = line.split.map {|x| x = {'agent' => 'minecraft', 'name' => x}}
 
-				http = Net::HTTP.new(uri.host, uri.port)
-				http.use_ssl = true
-				# I don't think we have to distrust the mojang server, no matter how dumb mojang is
-				# and this eliminates a bunch of annoying warnings that the client will start printing
-				http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+			http = Net::HTTP.new(uri.host, uri.port)
+			http.use_ssl = true
+			# I don't think we have to distrust the mojang server, no matter how dumb mojang is
+			# and this eliminates a bunch of annoying warnings that the client will start printing
+			http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-				request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
-				request.body = json_body
+			request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
+			request.body = JSON.generate(query_list)
 
-				response = http.request(request)
+			response = http.request(request)
+			profile = JSON.parse(response.body)
 
-				profile = JSON.parse(response.body)
+			# We found some profiles associated with these users if the returned size is larger than 0
+			if profile['size'] > 0
+				profile['profiles'].inject(linked_uuids) {|result, element| result << element['id']}
 
-				unless profile['profiles'].empty?
-					linked_uuids << profile['profiles'][0]['id']
-					puts "#{account} : #{profile['profiles'][0]['id']}"
+				unless linked_uuids.empty?
+					output.puts(linked_uuids.join(' '))
+					puts "#{associations} association lists converted"
+					associations += 1
 				end
-			end
-
-			unless linked_uuids.empty?
-				line = linked_uuids.join(' ')
-				output.puts(line)
 			end
 		end
 	end
